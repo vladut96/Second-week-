8a7ecccc25f42fd420c0770fa0db250b147b2725
vladut96 <paygrishenko96@gmail.com>
2025-02-17 18:10:11 +0100

    Week 2, task completed.

--- src/routes/testing-router.ts
+++ src/routes/testing-router.ts
@@ -3,7 +3,7 @@ import {db} from "../db/db"; // Import the data arrays
 
 export const testingRouter = Router();
 
-testingRouter.delete("/all-data", (req: Request, res: Response) => {
+testingRouter.delete("/", (req: Request, res: Response) => {
     db.blogs = [];
     db.posts = [];
 
