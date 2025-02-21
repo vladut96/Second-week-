6ae5a83f9fedd294b025d7bc897e185bf737bf16
vladut96 <paygrishenko96@gmail.com>
2025-02-21 18:58:30 +0100

    Week 3, task completed. Connected to Mongo DB

--- src/settings.ts
+++ src/settings.ts
@@ -10,5 +10,4 @@ export const SETTINGS = {
         BLOGS: '/blogs',
     },
     MONGO_URL: 'mongodb+srv://admin:admin@learning.7fe0l.mongodb.net/my-database?retryWrites=true&w=majority&appName=Learning',
-
 }
