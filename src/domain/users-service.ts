d5978bd775eb3a3afb84504aa2afebf861ee9861
vladut96 <paygrishenko96@gmail.com>
2025-06-24 14:46:00 +0200

    Week 10, tasks completed. Moved to classes

--- src/domain/users-service.ts
+++ src/domain/users-service.ts
@@ -1,14 +1,19 @@
-import {UsersRepository} from '../Repository/usersRepository';
+import { injectable, inject } from 'inversify';
+import { UsersRepository } from '../Repository/usersRepository';
 import { hashPassword } from '../utils/passwordUtils';
-import {UserViewModel, Paginator, UserInputModel} from '../types/types';
-import {EmailConfirmationFactory} from "../../service/email-confirmation-code-generator";
+import { UserViewModel, Paginator, UserInputModel } from '../types/types';
+import { EmailConfirmationFactory } from "../../service/email-confirmation-code-generator";
 
+@injectable() // Add this decorator
 export class UsersService {
-    constructor(protected usersRepository: UsersRepository) {
-    }
+    constructor(
+        @inject(UsersRepository) protected usersRepository: UsersRepository // Add @inject
+    ) {}
+
     async getUsers({ searchLoginTerm, searchEmailTerm, sortBy, sortDirection, pageNumber, pageSize }: { searchLoginTerm?: string; searchEmailTerm?: string; sortBy: string; sortDirection: 1 | -1; pageNumber: number; pageSize: number; }): Promise<Paginator<UserViewModel>> {
         return await this.usersRepository.getUsers({ searchLoginTerm, searchEmailTerm, sortBy, sortDirection, pageNumber, pageSize });
     }
+
     async createUser(userData: UserInputModel) {
         const existingUser = await this.usersRepository.getUserByLoginOrEmail(userData.login, userData.email);
         if (existingUser) {
@@ -27,13 +32,13 @@ export class UsersService {
             passwordHash,
             emailConfirmation: EmailConfirmationFactory.createDefault(),
             passwordRecovery: {
-                recoveryCode:  null,
-                expirationDate:  null
+                recoveryCode: null,
+                expirationDate: null
             }
         });
     }
+
     async deleteUserById(userId: string): Promise<boolean> {
         return await this.usersRepository.deleteUserById(userId);
     }
 }
-
